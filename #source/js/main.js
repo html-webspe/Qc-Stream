a535f9a2c6ada8bfdae10f0b7c72705d57de7420
hron2017 <andreypbiz@gmail.com>
2021-08-18 22:01:53 +0300

    commit

--- #source/js/main.js
+++ #source/js/main.js
@@ -1,5 +1,4 @@
 //@prepros-append jq-start.js
-//@prepros-append components/modal.js
 //@prepros-append script.js
 
 //@prepros-append jq-end.js
--- #source/js/vendors.js
+++ #source/js/vendors.js
@@ -1,3 +1,1 @@
-//@prepros-append libs/slick.min.js
-//@prepros-append components/dynamic_adapt.js
 
